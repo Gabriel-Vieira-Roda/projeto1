1806fbe45abea77f233c19a9e6310e6c5f0ae549
Gabriel Vieira Roda <gabriel.barrionuevo94@gmail.com>
2022-07-02 16:03:51 -0300

    Versão3

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a;
+	printf("coloque um numero: ");
+	scanf("%d", a);
+	if(a > 6){printf("maior que 6");}else{printf("menor que 6");}
+return 0;
+}
